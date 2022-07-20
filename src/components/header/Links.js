ec9158dea4146bc25c4356919cea82946441d265
pjainxido <pjainxido@gmail.com>
2022-07-20 21:53:57 +0900

    Update Links.js

--- src/components/header/Links.js
+++ src/components/header/Links.js
@@ -1,17 +1,9 @@
 import React from "react"
-import { Link } from "gatsby"
-import { FaGithubSquare, FaHome } from "react-icons/fa"
+import { FaGithubSquare } from "react-icons/fa"
 
 const Links = ({ contacts }) => {
   return (
     <div className="left-links float-right">
-      <a className="text-light">
-        <Link to="/">
-          <span className="d-block py-1 link-dark" title="Home">
-            <FaHome size={40} style={{ color: '#fff' }} />
-          </span>
-        </Link>
-      </a>
       <a className="text-light" href={contacts.github}>
         <span title="GitHub">
           <FaGithubSquare size={40} style={{ color: "light" }} />
